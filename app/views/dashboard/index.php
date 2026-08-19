1e4a744471d85b59192a2dc460953dca4e12aa54
lazycoder12 <restyg931@gmail.com>
2026-08-19 13:19:42 +0800

    Fixed ci bugs

--- app/views/dashboard/index.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'index')
-
-@section('content')
-    <h1>Welcome to dashboard.index</h1>
-    <p>This template was created by the Trip CLI.</p>
-@endsection
